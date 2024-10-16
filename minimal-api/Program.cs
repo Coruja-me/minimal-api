14710784a57990294f68f790f295dc9866643d1a
Coruja <cauavalves@gmail.com>
2024-10-15 22:31:24 -0300

    update"Configurações e pastas"

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,16 @@
+using minimal_api.Domain.DTO;
+using minimal_api.Infrastructure.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "admin")
+        return Results.Ok("Login realizado com sucesso!");
+    else
+        return Results.Unauthorized();
+});
 
 app.Run();
